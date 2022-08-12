149108ba1922b61ec143e50b215a560847751947
just-benedict-it <shsms9223@gmail.com>
2022-08-12 21:11:55 +0900

    Updates

---
+++ precache-manifest.eabae9da24b083d7cc017d89a692eabc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "752f1284bb648c171ac3",
+    "url": "/react-movie.git/static/js/main.752f1284.chunk.js"
+  },
+  {
+    "revision": "288f537e1cdbc1f4d848",
+    "url": "/react-movie.git/static/js/1.288f537e.chunk.js"
+  },
+  {
+    "revision": "abdd3fc07a849d0a7103",
+    "url": "/react-movie.git/static/js/runtime~main.abdd3fc0.js"
+  },
+  {
+    "revision": "9b4160aeb775ee95d6ba45479017a9b2",
+    "url": "/react-movie.git/index.html"
+  }
+];
