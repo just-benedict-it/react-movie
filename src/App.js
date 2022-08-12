95d77c86098abc8c87c79dbca3a5e74f81354119
just-benedict-it <shsms9223@gmail.com>
2022-08-12 22:49:08 +0900

    package:changed2

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
+    // <Router basename={process.env.PUBLIC_URL}>
+    //   <Routes>
+    //     <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}></Route>
+    //     <Route
+    //       path={`${process.env.PUBLIC_URL}/movie/:id`}
+    //       element={<Detail />}
+    //     ></Route>
+    //   </Routes>
+    // </Router>
     <Router basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}></Route>
-        <Route
-          path={`${process.env.PUBLIC_URL}/movie/:id`}
-          element={<Detail />}
-        ></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/movie/:id" element={<Detail />}></Route>
       </Routes>
     </Router>
   );
